b89ec5113122ad6286ab3357010561ddb3c546bd
Ian Cantor <icantor@fandm.edu>
2019-01-26 12:04:15 -0500

    Implements player hiding and movement toggling

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+enum STATE {MOVE, HIDE}
+
+onready var current_state = STATE.MOVE
+
 # Temporary draw override for visual representation of player
 # Remove this when wave assets are added
 func _draw():
@@ -9,14 +13,30 @@ func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
+	# MOVEMENT
 	if Input.is_action_pressed("ui_down"):
+		if current_state == STATE.HIDE:
+			return
 		self.position.y += 1
 
 	elif Input.is_action_pressed("ui_left"):
+		if current_state == STATE.HIDE:
+			return
 		self.position.x -= 1
 
 	elif Input.is_action_pressed("ui_right"):
+		if current_state == STATE.HIDE:
+			return
 		self.position.x += 1
 
 	elif Input.is_action_pressed("ui_up"):
+		if current_state == STATE.HIDE:
+			return
 		self.position.y -= 1
+
+	# HIDING
+	if Input.is_action_pressed("ui_select"):
+		current_state = STATE.HIDE
+
+	elif Input.is_action_just_released("ui_select"):
+		current_state = STATE.MOVE
